18ff6a55673115039e9e5c28bdfb45c7ee8146a7
Pawel Stanecki <p.stanecki93@gmail.com>
2021-05-15 14:51:46 +0200

    Added basic navigation

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { StatusBar, setStatusBarTranslucent } from 'expo-status-bar';
-import { FlatList, Platform, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { FlatList, Platform, SafeAreaView, SectionList,StyleSheet, Text, View } from 'react-native';
 import { API_TOKEN } from "react-native-dotenv";
 import useInterval from './useInterval'
 import _ from 'lodash';
+import { NavigationContainer  } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 const COMPETITIONS_ID: {
   BRASIL_SERIE_A: {id: 2013,name: "Série A"},
@@ -27,39 +29,48 @@ interface ICompetitions {
 
 const fromSeconds = (seconds: number): number => 1000 * seconds;
 
-const renderMatch = ({ item }) => (
-  <Text key={item.id}>{item?.homeTeam.name} - {item?.awayTeam.name}</Text>
-);
+const MatchScreen = ({ navigation }) => <View><Text>aksjflkajsflahslfhashflijh</Text></View>
 
-const renderItem = ({ item }) => {
-  console.log({item})
-  return (
-  <>
-   <Text style={styles.text}key={`${item[0]}-title`}>{item[0]}</Text>
-  <FlatList
-        data={item[1]}
-        renderItem={renderMatch}
-        keyExtractor={item => `${item?.homeTeam.name}-${item?.awayTeam.name}`}
-      />
-      
-</>
-)};
-
-
-export default function App() {
-  // The counter
+const HomeScreen = ({ navigation }) => {
   const [competitions, setcompetitions] = useState<ICompetitions[]>([])
 
   useEffect(    () => {
     setStatusBarTranslucent(false)
     // Your custom logic here
-  fetch('http://api.football-data.org/v2/matches?plan=TIER_ONE', { headers:{'X-Auth-Token': API_TOKEN}})
+  fetch('http://api.football-data.org/v2/matches?plan=TIER_ONE', { headers:{'X-Auth-Token': API_TOKEN}}) // put into localstorage and do not refetch before 1min
     .then((response) => response.json())
     .then((json) =>setcompetitions(_.groupBy(json.matches, m=>m?.competition?.name)))
     .catch((error) => console.error(error))
     .finally(() => console.log({competitions}))
 
   },[])
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Competitions</Text>
+      <SectionList 
+      sections={Object.entries(competitions).map(([key, value])=> ({title: key, data: value}))}
+        renderItem={({item}) => 
+          <>
+           <Text key={`${item}-title`}  onPress={() => {
+                  /* 1. Navigate to the Details route with params */
+                  navigation.navigate('Match', {
+                    itemId: item?.id,
+                    otherParam: 'anything you want here',
+                  });
+                }}>{item?.homeTeam.name} - {item?.awayTeam.name}</Text>
+        </>}
+        renderSectionHeader={({section}) => <Text style={styles.text}>{section.title}</Text>}
+        keyExtractor={(item, index) => index}
+      />
+      <StatusBar style="auto" />
+    </SafeAreaView >
+  );
+}
+
+const Stack = createStackNavigator();
+
+export default function App() {
 
 
   // useInterval(
@@ -77,15 +88,12 @@ export default function App() {
   // )
   
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Competitions</Text>
-      {<FlatList
-        data={Object.entries(competitions)}
-        renderItem={renderItem}
-        keyExtractor={item => item[0].toString()}
-      />}
-      <StatusBar style="auto" />
-    </SafeAreaView >
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Match" component={MatchScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
